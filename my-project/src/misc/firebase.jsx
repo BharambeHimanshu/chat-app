a92ef376f1f14e1a4f60e9b30c56a8e7e8c77df0
Himanshu Bharambe <himanshubharambe04@gmail.com>
2022-12-04 10:04:29 +0530

    add firebase configration

---
+++ my-project/src/misc/firebase.jsx
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+const config = {
+    apiKey: "AIzaSyAo5rdmUyequGxqMG76-_bxawBAgS-_KLQ",
+    authDomain: "chat-box-83925.firebaseapp.com",
+    databaseURL: "https://chat-box-83925-default-rtdb.firebaseio.com",
+    projectId: "chat-box-83925",
+    storageBucket: "chat-box-83925.appspot.com",
+    messagingSenderId: "423384450679",
+    appId: "1:423384450679:web:ccebd10983fd3abd79a872"
+  };
+
+const app = firebase.initializeApp(config);
